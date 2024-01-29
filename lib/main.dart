0e3cea98c9bb17dfba3c809d356209fb5bab2ab6
FardanAthilla <fardanathilla.gmail.com>
2024-01-29 09:33:44 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:crud_firebase/firebase/firebase_options.dart';
+import 'package:crud_firebase/Page/homepage.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: const HomePage(),
+    );
+  }
+}
